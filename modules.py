ccfdc7c700749fe65520e2af98c9a571dbaada22
CristianGeovannyRodriguezMejia <cr3389705@gmail.com>
2026-07-08 21:18:53 -0600

    primer push

---
+++ modules.py
@@ -0,0 +1,17 @@
+import os ;
+
+def Menu()-> int :
+    opciones = """" 
+    Opciones a Elegir
+    1.Mostrar Contenido
+    2.Inserta un Reguistro
+    3.Modificar Reguistro
+    4.Eliminar Reguistro
+    5.Salir
+    Digita la opcion :
+    """
+    opc =int(input(opciones))
+    return opc
+
+def MostrarContenido()-> None :
+    
