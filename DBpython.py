55cda51d6bbbed99582abe83ac4834e7507f37d0
CristianGeovannyRodriguezMejia <CristianGeovannyRodriguezMejia@users.noreply.github.com>
2026-07-10 19:16:12 -0600

    conexion exitosa

--- DBpython.py
+++ DBpython.py
@@ -1,9 +1,15 @@
-import mysql.connector as sql
+import pymysql as sql
 
-connexion = sql.connect(
-        host="",
-        port=3306,
-        database="",
-        user="integrador",
-        password="RETOS"
+
+try :
+    connexion = sql.connect(
+            host="192.168.1.77",
+            port=3306,
+            database="banco",
+            user="integrador",
+            password="RETOS"
     )
+    print("Se conecto ... Creo")
+
+except sql.Error as error :
+    print(f"Pos algo fallo ... Creo {error}")
