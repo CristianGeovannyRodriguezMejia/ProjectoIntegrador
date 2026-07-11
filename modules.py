68a814f2e4102225e9a64971c7eda69b7de37057
CristianGeovannyRodriguezMejia <CristianGeovannyRodriguezMejia@users.noreply.github.com>
2026-07-11 07:37:24 -0600

    creacion el insert

--- modules.py
+++ modules.py
@@ -1,4 +1,4 @@
-import pymysql;
+import pymysql
 from DBpython import *;
 def Menu()-> int :
     opciones = """" 
@@ -15,12 +15,39 @@ def Menu()-> int :
 
 def InsetarContenido()-> None :
      
-     idpersona =txt1.get()
-     nombre =txt2.get();
-     edad =txt.get();
-     sqlsentence ="";
-     columnas = ();
-     connexion=pymysql.connect(host='',user='',password='',database='')
-     curso=connexion.cursor();
-     resul=curso.execute(sqlsentence, columnas)
+
+    id_transaccion =input("Digite el id  : ")
+    tipo =input("Ingrese el tipo transaccion Deposito/Retiro/Tranferencia : ");
+    cuenta_origen =input("digite la cuenta de origen : ");
+    cuenta_destino =input("digite la cuenta destino : ");
+    monto =input("digite el monto : ");
+    descripcion=input("descricion de tranferencia : ");
+    usuario = input("Ingrese su nombre de ususario :")
+    try :
+        connexion=pymysql.connect(host='192.168.1.77'
+                                  ,port=3306,
+                                  user='integrador'
+                                  ,password='RETOS'
+                                  ,database='banco'
+                                  )
+        ps =connexion.cursor();
+   
+        sqlsentence ="INSEtipo =txt3.get();RT INTO transacciones(id_transaccion,fecha,tipo,cuenta_origen,cuenta_destino,monto,descripcion,usuario,) VALUES (%s,NOW(),%s,%s,%s,%s,%s,%s)";
+        columnas = (id_transaccion,tipo,cuenta_origen,cuenta_destino,monto,descripcion,usuario,);
+
+    
+        ps.execute(sqlsentence,columnas)
+        connexion.commit()
+        print("Creo que se reguistro con exito")
+    
+    except pymysql.Error as fallo :
+        print(f"Nose que paso pero : {fallo}")
+        #Esto cancela la ejecucion si falla algo pongo esto por que no sabia que existia XD
+        if connexion in locals(): connexion.rollback();
+    finally :
+        #pos esto hace lo que dice los cierra
+       if ps in locals() : ps.close()
+       if connexion in locals() : connexion.close()
+    
+    
     
