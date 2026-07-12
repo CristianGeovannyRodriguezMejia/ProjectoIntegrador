c0f79fc5fb8e9e9cf77a3318054faf883942f86c
CristianGeovannyRodriguezMejia <CristianGeovannyRodriguezMejia@users.noreply.github.com>
2026-07-12 11:16:23 -0600

    ya

--- Main.py
+++ Main.py
@@ -9,19 +9,19 @@ while opc != 5 :
     opc=Menu();
     match(opc):
      case 1 :
-      pass
+         print("\n ---Reguistros---")
+         if not fila :
+            print("No hay pe");
+         else :  
+            for filas in fila :
+               print(filas);
+            
+         input("PAUSA PE")
      case 2:
         insert.append(InsetarContenido());
         input("PAUSA PE");
      case 3:
-            print("\n ---Reguistros---")
-            if not fila :
-               print("No hay pe");
-            else :  
-               for filas in fila :
-                  print(filas);
-            
-            input("PAUSA PE")
+         pass
      case 4:
         delete.append(EliminarContenido());
      case 5 :
